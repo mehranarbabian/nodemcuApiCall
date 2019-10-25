95a699cc9633cb2210dee7e640bdbcd3a056e8a0
mehranarbabian <mehranarbabian76@google.com>
2019-10-25 19:06:13 +0330

    Initial commit

---
+++ sketch_oct22b.ino
@@ -0,0 +1,38 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+
+
+const char* ssid = "mehran.3";
+const char* password = "hogabr98";
+String order="off";
+
+void setup() 
+{
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    delay(1000);
+    Serial.println("Connecting...");
+  }
+}
+
+void loop() 
+{
+  if (WiFi.status() == WL_CONNECTED) 
+  {
+    HTTPClient http; //Object of class HTTPClient
+    http.begin("http://aftermirror.ir/returnProceccedRequest");
+   
+    int httpCode = http.GET();
+    if (httpCode > 0) 
+    {
+    order = http.getString();
+Serial.println(order);
+
+
+    }
+    http.end(); //Close connection
+  }
+  delay(4000);
+}
